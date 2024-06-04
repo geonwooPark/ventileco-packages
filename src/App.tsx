1dcb0b2095227e93c6ea3947a07b3b5474453013
geonwooPark <white0581@naver.com>
2024-06-04 15:13:17 +0900

    Chore: 기타 코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -14,71 +14,16 @@ function App() {
 
   const onSubmit = (data: any) => console.log(data)
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <CheckBoxGroup defaultValues={['January']}>
-          <CheckBoxTitle>CheckBox</CheckBoxTitle>
-          <CheckList className="flex gap-2">
-            <CheckBoxItem value="January" register={register('months')}>
-              {({ isSelected }) => (
-                <div
-                  className={`${isSelected && 'text-active'} cursor-pointer rounded-md border border-black px-3 py-2`}
-                >
-                  <div className="flex justify-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="size-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                      />
-                    </svg>
-                  </div>
-                  <p>January</p>
-                </div>
-              )}
-            </CheckBoxItem>
-            <CheckBoxItem value="February" register={register('months')}>
-              {({ isSelected }) => (
-                <div
-                  className={`${isSelected && 'text-active'} cursor-pointer rounded-md border border-black px-3 py-2`}
-                >
-                  <div className="flex justify-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="size-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                      />
-                    </svg>
-                  </div>
-                  <p>February</p>
-                </div>
-              )}
-            </CheckBoxItem>
-          </CheckList>
-        </CheckBoxGroup>
-        <input type="submit" />
-      </form>
+  const checkBoxRegister = register('months', {
+    required: '적어도 한 개를 선택해주세요.',
+  })
 
-      <CheckBoxGroup defaultValues={values} setValues={setValues}>
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <CheckBoxGroup defaultValues={['January']}>
         <CheckBoxTitle>CheckBox</CheckBoxTitle>
         <CheckList className="flex gap-2">
-          <CheckBoxItem value="January">
+          <CheckBoxItem value="January" register={checkBoxRegister}>
             {({ isSelected }) => (
               <div
                 className={`${isSelected && 'text-active'} cursor-pointer rounded-md border border-black px-3 py-2`}
@@ -103,7 +48,7 @@ function App() {
               </div>
             )}
           </CheckBoxItem>
-          <CheckBoxItem value="February">
+          <CheckBoxItem value="February" register={checkBoxRegister}>
             {({ isSelected }) => (
               <div
                 className={`${isSelected && 'text-active'} cursor-pointer rounded-md border border-black px-3 py-2`}
@@ -130,7 +75,8 @@ function App() {
           </CheckBoxItem>
         </CheckList>
       </CheckBoxGroup>
-    </div>
+      <input type="submit" />
+    </form>
   )
 }
 
--- src/components/CheckBox/CheckBox.stories.tsx
+++ src/components/CheckBox/CheckBox.stories.tsx
@@ -82,12 +82,16 @@ export function WithReactHookForm() {
 
   const onSubmit = (data: any) => console.log(data)
 
+  const checkBoxRegister = register('months', {
+    required: 'You must select at least one',
+  })
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <CheckBoxGroup defaultValues={['January']}>
         <CheckBoxGroup.Title>With React Hook Form</CheckBoxGroup.Title>
         <CheckBoxGroup.List className="flex gap-2">
-          <CheckBoxGroup.Item value="January" register={register('months')}>
+          <CheckBoxGroup.Item value="January" register={checkBoxRegister}>
             {({ isSelected }) => (
               <div
                 className={`${isSelected && 'text-active'} cursor-pointer rounded-md border border-black px-3 py-2`}
@@ -112,7 +116,7 @@ export function WithReactHookForm() {
               </div>
             )}
           </CheckBoxGroup.Item>
-          <CheckBoxGroup.Item value="February" register={register('months')}>
+          <CheckBoxGroup.Item value="February" register={checkBoxRegister}>
             {({ isSelected }) => (
               <div
                 className={`${isSelected && 'text-active'} cursor-pointer rounded-md border border-black px-3 py-2`}
