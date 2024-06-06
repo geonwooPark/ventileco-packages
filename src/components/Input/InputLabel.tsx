9afa0926d0a5004f1831bcbeae7359670eb72b35
박건우 <white0581@naver.com>
2024-06-06 20:54:41 +0900

    Chore: 오류 수정 후 재배포

--- src/components/Input/InputLabel.tsx
+++ src/components/Input/InputLabel.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useContext } from 'react'
+import { PropsWithChildren, useContext } from 'react'
 import { InputContext } from './Input'
 
 interface InputLabelProps {
