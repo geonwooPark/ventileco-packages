339ee1f44428f297ae2b480a3ab0426724e924bf
박건우 <white0581@naver.com>
2024-06-12 21:38:34 +0900

    Feat: custom createContext 함수 제작 및 기능 캡슐화

---
+++ src/utils/_createContext.ts
@@ -0,0 +1,17 @@
+import React, { createContext } from 'react'
+
+export const _createContext = <T extends Record<string, unknown>>() => {
+  const Context = createContext<T | null>(null)
+
+  const useContext = () => {
+    const context = React.useContext(Context)
+
+    if (context === null) {
+      throw Error('Error')
+    }
+
+    return context
+  }
+
+  return [useContext, Context.Provider] as const
+}
