f455225aad0f6bd56071406cfdc401e2d358c4c8
박건우 <white0581@naver.com>
2024-06-30 17:38:15 +0900

    Test: Box 컴포넌트 테스트 케이스 추가

--- src/components/Box/Box.test.tsx
+++ src/components/Box/Box.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react'
+import Box from './Box'
 import { Normal } from './Box.stories'
 
 describe('Box', () => {
-  it('a태그로 Box 컴포넌트가 렌더링되는지 확인', () => {
+  it('Box 컴포넌트가 button 태그로 렌더링되는지 확인', () => {
+    render(<Box as="button" />)
+    expect(screen.getByRole('button')).toBeInTheDocument()
+  })
+  it('Box 컴포넌트가 a태그로 렌더링되는지 확인', () => {
     render(<Normal />)
     expect(screen.getByRole('link')).toBeInTheDocument()
   })
