d8c131481ded469643105da0b183b5345f9255e8
박건우 <white0581@naver.com>
2024-06-30 17:53:31 +0900

    Test: Button 컴포넌트 테스트 케이스 추가

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,9 +1,23 @@
 import { render, screen } from '@testing-library/react'
-import { Normal } from './Button.stories'
+import Button from './Button'
 
 describe('Button', () => {
-  it('Normal 버튼 컴포넌트가 렌더링되는지 확인', () => {
-    render(<Normal />)
-    expect(screen.getByRole('button')).toBeInTheDocument()
+  it('버튼 컴포넌트가 렌더링되는지 확인', () => {
+    render(<Button type="button" data-testid="test-normal" />)
+    const button = screen.getByTestId('test-normal')
+
+    expect(button).toBeInTheDocument()
+  })
+  it('버튼 컴포넌트의 disabled Props 동작 테스트 ', () => {
+    render(<Button type="button" disabled={true} data-testid="test-disabled" />)
+    const button = screen.getByTestId('test-disabled')
+
+    expect(button).toBeDisabled()
+  })
+  it('버튼 컴포넌트의 isLoading Props 동작 테스트 ', () => {
+    render(<Button type="button" isLoading={true} />)
+    const spinner = screen.getByTestId('test-spinner')
+
+    expect(spinner).toBeInTheDocument()
   })
 })
