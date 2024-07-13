a8e0b954df4e9e048a3b1700577c3e26f0c31869
박건우 <white0581@naver.com>
2024-07-13 18:50:32 +0900

    Feat: Toast 컴포넌트 Framer Motion 애니메이션 적용

--- src/components/Toast/ToastWrapper.tsx
+++ src/components/Toast/ToastWrapper.tsx
@@ -3,10 +3,32 @@ import { useToastContext } from './ToastProvider'
 
 interface ToastUIProps {
   id: number
+  motion?: any
+  animationProps?: object
 }
 
-function ToastWrapper({ children, id }: PropsWithChildren<ToastUIProps>) {
+function ToastWrapper({
+  children,
+  id,
+  motion,
+  animationProps,
+}: PropsWithChildren<ToastUIProps>) {
   const { removeToast } = useToastContext()
+
+  const Component = motion ? motion['div'] : 'div'
+
+  const props = motion
+    ? animationProps || {
+        variants: {
+          hidden: { opacity: 0, y: 20 },
+          visible: { opacity: 1, y: 0 },
+        },
+        initial: 'hidden',
+        animate: 'visible',
+        viewport: { once: true },
+        transition: { duration: 0.3 },
+      }
+    : {}
 
   const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
     if (e.key === 'Enter' || e.key === ' ') {
@@ -17,16 +39,17 @@ function ToastWrapper({ children, id }: PropsWithChildren<ToastUIProps>) {
   const toastWrapperStyle = useMemo(() => ({ cursor: 'pointer' }), [])
 
   return (
-    <div
+    <Component
       role="alert"
       tabIndex={0}
       aria-atomic="true"
       onClick={() => removeToast(id)}
       onKeyDown={onKeyDown}
       style={toastWrapperStyle}
+      {...props}
     >
       {children}
-    </div>
+    </Component>
   )
 }
 
