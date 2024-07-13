38205bf49c239de883f4882984e5a749585360cc
박건우 <white0581@naver.com>
2024-07-13 18:53:49 +0900

    Chore: Framer Motion 설치 및 기타 코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,14 @@
-import { useState } from 'react'
-import Counter from './components/Counter/CounterMain'
+import { useToast } from './components'
 
 function App() {
-  const [value, setValue] = useState(0)
-  console.log('🚀 ~ App ~ value:', value)
+  const addToast = useToast()
 
   return (
-    <Counter value={value} setValue={setValue} maximum={10} minimum={-4}>
-      <div className="flex items-center gap-2">
-        <Counter.Number className="h-8 w-20 rounded-md border border-gray-700 text-center text-2xl" />
-        <div className="flex flex-col">
-          <Counter.Up>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m4.5 15.75 7.5-7.5 7.5 7.5"
-              />
-            </svg>
-          </Counter.Up>
-          <Counter.Down>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m19.5 8.25-7.5 7.5-7.5-7.5"
-              />
-            </svg>
-          </Counter.Down>
-        </div>
-      </div>
-    </Counter>
+    <div>
+      <button onClick={() => addToast.success('성공 메시지')}>버튼</button>
+      <button onClick={() => addToast.error('실패 메시지')}>버튼</button>
+      <button onClick={() => addToast.info('인포 메시지')}>버튼</button>
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import ToastProvider from './components/Toast/ToastProvider'
+import { motion } from 'framer-motion'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -12,7 +13,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <App />
           <ToastProvider.List>
             {toasts.map((toast) => (
-              <ToastProvider.Wrapper key={toast.id} id={toast.id}>
+              <ToastProvider.Wrapper
+                key={toast.id}
+                motion={motion}
+                id={toast.id}
+              >
                 <div className="flex h-[80px] w-[320px] items-center rounded-full border px-4 py-2">
                   <div
                     className={`mr-2 size-5 shrink-0 ${toast.type === 'success' ? 'bg-success' : toast.type === 'error' ? 'bg-error' : 'bg-info'}`}
